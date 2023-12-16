757cc3b2a3f077c9c9eb1390f16fbd41615cf9d4
Virginia <150845139+VPaigePat@users.noreply.github.com>
2023-12-16 14:41:07 -0500

    updated javascript and HTML

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ function updateCity(event){
     <div class="city">
         <div>
           <h2>${cityName}</h2>
-          <div class="date">${cityTime.format("MMMM do YYYY")}</div>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
         <div class="time"> ${cityTime.format("h:mm:ss")}<small>${cityTime.format(
     "A"
