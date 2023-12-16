bc56be175303e892e9430627995df8641407c65f
Virginia <150845139+VPaigePat@users.noreply.github.com>
2023-12-16 13:49:50 -0500

    Updated Javascript
    Added function to select a specific city from the drop down and displayed it's time and date on the screen

--- src/index.js
+++ src/index.js
@@ -20,3 +20,26 @@ tokyoDateElement.innerHTML = tokyoTimeZone.format("MMMM Do YYYY")
 }
 
 setInterval(updateTime, 1000);
+
+function updateCity(event){
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment.tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+    <div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM do YYYY")}</div>
+        </div>
+        <div class="time"> ${cityTime.format("HH:mm:ss")}</div>
+      </div>
+      
+      `
+    
+
+}
+
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
